af8c2ae4afa164cdb330245e21c84cfa017695f0
Büşra Akay <61781887+busraakay@users.noreply.github.com>
2025-06-06 14:17:14 +0300

    Project added.

---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        public readonly IReservationService reservationService;
+        public ResController(IReservationService ReservationService)
+        {
+            reservationService = ReservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return reservationService.GetResByBkgNumber(Id);
+        }
+    }
+
+}
